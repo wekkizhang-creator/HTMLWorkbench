4a2d3c4640004a12c07b578645bd5c176e7d9fb6
wekkizhang-creator <wekkizhang@gmail.com>
2026-05-30 17:49:23 +0800

    Use uploaded brand logo

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const MIME_TYPES = new Map([
   [".html", "text/html; charset=utf-8"],
   [".js", "text/javascript; charset=utf-8"],
   [".json", "application/json; charset=utf-8"],
+  [".png", "image/png"],
   [".svg", "image/svg+xml; charset=utf-8"],
   [".txt", "text/plain; charset=utf-8"]
 ]);
